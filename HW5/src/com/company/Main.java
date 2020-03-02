972adfea74de8b669a8a8777fbd521d5cdcbc4f1
AnastasiiaFil <anastasiiafilipchuk@gmail.com>
2020-03-02 22:43:06 +0200

    Added absract collections

--- HW5/src/com/company/Main.java
+++ HW5/src/com/company/Main.java
@@ -1,50 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Cat c1 = new Cat(10, 3, "White", true);
-        Cat c2 = new Cat(5, 4, "Gray", true,"Jin");
-        Dog d = new Dog(15, 3.5, "Black", true);
-        Fish f = new Fish(1, 1, "Gold", false);
-        Hamster h = new Hamster(1, 0.3, "Brown", true);
-        Wolf w = new Wolf(5,20,"Gray", true);
-        Lion l = new Lion(3,25,"Yellow",true);
-        Giraffe g = new Giraffe(1, 100, "Mixed",false);
-        Crocodile croc =  new Crocodile(2,120,"Green",true);
-        guideDog gd = new guideDog(5,15,"Brown","Lucky",true,true);
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Cat(10, 3, "White", true));
+        animals.add(new Cat(5, 4, "Gray", true,"Jin"));
+        animals.add(new Dog(15, 3.5, "Black", true));
+        animals.add(new Fish(1, 1, "Gold", false));
+        animals.add(new Hamster(1, 0.3, "Brown", true));
+        animals.add(new Wolf(5,20,"Gray", true));
+        animals.add(new Lion(3,25,"Yellow",true));
+        animals.add(new Giraffe(1, 100, "Mixed",false));
+        animals.add(new Crocodile(2,120,"Green",true));
+        guideDog gd = new guideDog(1,4,"Red","Bloo",true,false);
+        animals.add(gd);
 
-        c1.Voice();
-        c2.Voice();
-        d.Voice();
-        f.Voice();
-        h.Voice();
-        l.Voice();
-        w.Voice();
-        g.Voice();
-        croc.Voice();
-        gd.Voice();
+        for(Animal i : animals){
+            i.Voice();
+        }
+
         gd.takeHome();
 
         System.out.println("________________________________________________\n");
 
-        System.out.println(c1.toString());
-        System.out.println(c2.toString());
-        System.out.println(d.toString());
-        System.out.println(f.toString());
-        System.out.println(h.toString());
-        System.out.println(l.toString());
-        System.out.println(w.toString());
-        System.out.println(g.toString());
-        System.out.println(croc.toString());
-        System.out.println(gd.toString());
-
-
-
-
-
-
-
+        for(Animal i : animals){
+            System.out.println(i.toString());
+        }
     }
 }
